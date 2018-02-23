3a6f85b6c28808f4f037aca1564335a9664d52ea
Zongheng Yang <zongheng.y@gmail.com>
2018-02-23 00:49:57 +0000

    download_thirdparty.sh: use latest crc32c fix

--- src/thirdparty/download_thirdparty.sh
+++ src/thirdparty/download_thirdparty.sh
@@ -11,5 +11,6 @@ if [ ! -d $TP_DIR/arrow ]; then
   git clone https://github.com/concretevitamin/arrow.git "$TP_DIR/arrow"
 fi
 cd $TP_DIR/arrow
-# git fetch origin crc32c
-git checkout  62700c0c8f4ab64fd36b060669e3953f518f7b06
+git fetch origin
+# git checkout  62700c0c8f4ab64fd36b060669e3953f518f7b06
+git checkout d0c77948
